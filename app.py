b7f3ed67a49e8d16ae14b95763399ef997efbbfc
= <aishmeengangyan@gmail.com>
2025-04-09 18:36:37 +0530

    made app.py file

---
+++ app.py
@@ -0,0 +1,17 @@
+print('Hello')
+a=int(input("enter first number: "))
+b=int(input("enter second number: "))
+c=input("""
+    Enter type of operation to perform: 
+    a) 1 for addition 
+    b) 2 for substraction
+    c) 3 for multiplcation
+    d) 4 for division
+""")
+if c=="1":
+    # please make addition feature-> vinayak
+    addition(a , b)
+elif c=="2":
+    substraction(a , b)
+
+# elif c==
